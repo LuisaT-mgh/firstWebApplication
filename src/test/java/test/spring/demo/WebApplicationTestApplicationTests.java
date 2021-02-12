cf47abc2550e67a0fca91bf88d857cf021bf1c14
LuisaT-mgh <lui.thimm.19@lehre.mosbach.dhbw.de>
2021-02-12 16:35:33 +0100

    Initial commit

---
+++ src/test/java/test/spring/demo/WebApplicationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package test.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
